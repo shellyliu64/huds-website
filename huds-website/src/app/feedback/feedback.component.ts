d2db2424969d6173a6e3a42ef9d9b3dd3ef6f660
Shelly Liu <shelly_liu@college.harvard.edu>
2022-12-04 15:58:06 -0500

    adding files

---
+++ huds-website/src/app/feedback/feedback.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+
+@Component({
+  selector: 'app-feedback',
+  templateUrl: './feedback.component.html',
+  styleUrls: ['./feedback.component.css']
+})
+export class FeedbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
